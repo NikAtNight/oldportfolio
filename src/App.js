99c1383dce2f1340273714b7e5cacb20d2c76900
Nikhil <nikhil.kapadia.97@gmail.com>
2021-03-08 00:29:55 -0500

    Initial Portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Landing from './Components/Landing';
+import About from './Components/About';
+import Projects from './Components/Projects';
+import Contact from './Components/Contact';
+import Navigation from './Components/Navigation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    loading: true
+  }
+
+  componentDidMount() {
+    timeout().then(() => this.setState({ loading: false }))
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    if (loading) {
+      return null;
+    }
+
+    return (
+      <>
+        <Navigation />
+        <Landing />
+        <About />
+        <Projects />
+        <Contact />
+      </>
+    )
+  }
+}
+
+function timeout() {
+  return new Promise((resolve) => setTimeout(() => resolve(), 2500));
 }
 
 export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <header>
+            <div>
+
+            </div>
+            <div>
+                
+            </div>
+        </header>
+    )
+}
+
+export default Navigation;
